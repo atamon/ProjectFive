77b02524bf0e35899f858c3ff6c0350d8f633bf7
Victor Lindhé <victorlindhe@dhcp-184191.eduroam.chalmers.se>
2012-03-22 17:10:35 +0100

    det vi har på battlefield

---
+++ project/src/Model/ItemFactory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Victor Lindhé
+ */
+public class ItemFactory {
+    
+    public static Item createNewItem() {
+        return new Item();
+    }
+}
