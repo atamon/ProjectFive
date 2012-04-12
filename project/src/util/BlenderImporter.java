5b738fdc75408e5cf7d923b7abf80aca0cfa80e2
Anton Lindgren <lindgren.anton@gmail.com>
2012-04-12 17:41:07 +0200

    Modified Blender import so that it corrects ambient color not set in BlenderModelLoader from JME. Also configured some lighting to display the ambient and directional lighting better. Finally I removed the old BattlefieldTest that was left in another testpackage (Victor...).

--- project/src/util/BlenderImporter.java
+++ project/src/util/BlenderImporter.java
@@ -6,7 +6,10 @@ package util;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.asset.BlenderKey;
-import com.jme3.asset.BlenderKey.LoadingResults;
+import com.jme3.material.MatParam;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.plugins.blender.BlenderModelLoader;
@@ -25,14 +28,51 @@ public class BlenderImporter {
     public static Node loadModel(AssetManager assetManager, String path) {
 
         BlenderKey key = new BlenderKey(path);
-        Object obj = assetManager.loadAsset(key);
-        if (!(obj instanceof Node)) {
-            throw new RuntimeException("ERROR: BlenderImporter.loadModel()" + 
-                                       " got invalid path, no Spatial loaded." +
-                                       "Object recieved was of type: "
-                                        + obj.getClass());
+        Spatial blendResult = assetManager.loadAsset(key);
+        if (!(blendResult instanceof Node)) {
+            throw new RuntimeException("ERROR: BlenderImporter.loadModel()"
+                    + " got invalid path, no Spatial loaded."
+                    + "Object recieved was of type: "
+                    + blendResult.getClass());
         }
-        Node result = (Node) obj;
-        return result;
+        Node node = (Node) blendResult;
+
+        // Use this to correct the ambient color not correctly imported from Blender
+        correctMaterialColors(node);
+
+        return node;
+    }
+
+    /**
+     * BlenderModelLoader does not give us a usable AmbientColor to use, so we make our own.
+     * @param node The node holding the imported meshes.
+     */
+    private static void correctMaterialColors(Node node) {
+        // Get sub-nodes, we need to get to the materials so we can set ambient.
+        List<Spatial> children = node.getChildren();
+        Spatial subNode = children.get(0);
+        if (!(subNode instanceof Node)) {
+            throw new RuntimeException("ERROR: BlenderImporter.loadModel()"
+                    + " failed to get sub-node of blender result, got "
+                    + subNode.getClass());
+        }
+
+        // If node we can get the meshes and then the materials
+        List<Spatial> list = ((Node) subNode).getChildren();
+        for (Spatial spatial : list) {
+            if (spatial instanceof Geometry) {
+                Material mat = ((Geometry) spatial).getMaterial();
+                
+                // Get the diffuse-color from blender and set it to ambient
+                Object objColor = mat.getParam("Diffuse").getValue();
+                if (objColor instanceof ColorRGBA) {
+                    mat.setColor("Ambient", (ColorRGBA) objColor);
+                } else {
+                    throw new RuntimeException("ERROR: BlenderImporer.correctMaterialColors()" +
+                                " failed to set ambient color from diffuse color. Got obj of type: " +
+                                objColor.getClass());
+                }
+            }
+        }
     }
 }
