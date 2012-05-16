b2ef4d0f599cba15f005c78cc1f3c014f40ed769
Victor Lindhé <victorlindhe@gmail.com>
2012-05-16 14:50:22 +0200

    Solved lines of code stan problem

--- project/src/view/View.java
+++ project/src/view/View.java
@@ -4,16 +4,11 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.asset.AssetManager;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.material.RenderState;
-import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.Camera;
-import com.jme3.renderer.queue.RenderQueue;
-import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Node;
 import com.jme3.water.WaterFilter;
 import de.lessvoid.nifty.elements.Element;
@@ -47,9 +42,8 @@ public class View implements PropertyChangeListener {
     private Node rootNode;
     private GameState lastGameState;
     private RoundState lastRoundState;
-
     private GUIController guiControl;
-    
+
     public View(SimpleApplication jme3, IGame game,
             int windowWidth, int windowHeight, NiftyJmeDisplay niftyGUI) {
 
@@ -57,10 +51,10 @@ public class View implements PropertyChangeListener {
         this.game = game;
         this.assetManager = jme3.getAssetManager();
         this.rootNode = jme3.getRootNode();
-        
+
         // Create scene
         this.createScene();
-        
+
         // Register a BlenderLoader with our assetManager so it supports .blend
         BlenderImporter.registerBlender(assetManager);
 
@@ -72,11 +66,11 @@ public class View implements PropertyChangeListener {
         WaterFilter water = new WaterFilter(rootNode, new Vector3f(0, -1, 1));
         water.setWaterHeight(4f);
         waterPostProcessor.addFilter(water);
-        
-                
-        jme3.getViewPort().addProcessor(waterPostProcessor);        
+
+
+        jme3.getViewPort().addProcessor(waterPostProcessor);
         guiControl = new GUIController(niftyGUI, game, waterPostProcessor, jme3);
-        
+
         // We need to fetch first gamestates right away
         lastGameState = game.getState();
         lastRoundState = game.getRoundState();
@@ -95,27 +89,30 @@ public class View implements PropertyChangeListener {
     private void initGround(Vector size, Vector pos) {
         GraphicalBattlefield geoBattlefield = new GraphicalBattlefield(MonkeyConverter.convertToMonkey3D(size),
                 MonkeyConverter.convertToMonkey3D(pos), assetManager);
-        
+
         GraphicalBattlefield layerPlane = new GraphicalBattlefield(
-                new Vector3f(size.getX()+100, 0.1f, size.getZ()), 
-                new Vector3f(pos.getX()-50, 15f, pos.getZ()), assetManager);
-      /*  Material newMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        newMat.setTexture("ColorMap", assetManager.loadTexture("lines.png"));
-        newMat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
-        
-        layerPlane.getGeometry().setMaterial(newMat);
-        layerPlane.getGeometry().setQueueBucket(Bucket.Transparent);
-        rootNode.attachChild(layerPlane.getGeometry());*/
+                new Vector3f(size.getX() + 100, 0.1f, size.getZ()),
+                new Vector3f(pos.getX() - 50, 15f, pos.getZ()), assetManager);
+        /*
+         * Material newMat = new Material(assetManager,
+         * "Common/MatDefs/Misc/Unshaded.j3md"); newMat.setTexture("ColorMap",
+         * assetManager.loadTexture("lines.png"));
+         * newMat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+         *
+         * layerPlane.getGeometry().setMaterial(newMat);
+         * layerPlane.getGeometry().setQueueBucket(Bucket.Transparent);
+        rootNode.attachChild(layerPlane.getGeometry());
+         */
         rootNode.attachChild(geoBattlefield.getGeometry());
     }
-    
+
     private void initLighting() {
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(0, -1, 1));
         sun.setColor(ColorRGBA.White.mult(1.5f));
         rootNode.addLight(sun);
-        
-        
+
+
         AmbientLight ambient = new AmbientLight();
         ambient.setColor(ColorRGBA.White.mult(0.7f));
         rootNode.addLight(ambient);
@@ -134,7 +131,7 @@ public class View implements PropertyChangeListener {
 
         lastGameState = game.getState();
         lastRoundState = game.getRoundState();
-        
+
         if (lastRoundState == RoundState.PLAYING) {
             for (Element bar : hpBars.keySet()) {
                 Vector3f screenPos = jme3.getCamera().getScreenCoordinates(MonkeyConverter.convertToMonkey3D(hpBars.get(bar).getPosition()));
@@ -181,75 +178,90 @@ public class View implements PropertyChangeListener {
         rootNode.attachChild(gUnit.getNode());
     }
 
+    private void createPlayer(Player player) {
+        Unit unit = player.getUnit();
+
+        Vector pos = unit.getPosition();
+        Vector dir = unit.getDirection();
+        Vector size = unit.getSize();
+        // Also send playerID so it knows which unit to listen to.
+        int playerID = player.getId();
+
+        final ColorRGBA color = MonkeyConverter.convertToMonkeyColor(player.getColor());
+        this.createGraphicalUnit(playerID, pos, dir, size, color);
+
+        // If a player is created we need to start listening to it so we can know when it shoots
+        player.addPropertyChangeListener(this);
+
+        Element bar = guiControl.getHealthBarElement(playerID);
+        bar.getParent().setVisible(true);
+        hpBars.put(bar, game.getPlayer(playerID).getUnit());
+
+        // Show leave button
+        guiControl.playerActive(playerID, true);
+
+        if (game.hasValidAmountOfPlayers()) {
+            guiControl.gameValid(true);
+        }
+    }
+
+    private void removePlayer(int id) {
+        guiControl.playerActive(id, false);
+        if (!game.hasValidAmountOfPlayers()) {
+            guiControl.gameValid(false);
+        }
+    }
+
+    private void createCannonBall(CannonBall cannonBall) {
+        GraphicalCannonBall graphicalBall = new GraphicalCannonBall(ColorRGBA.Orange,
+                MonkeyConverter.convertToMonkey3D(cannonBall.getPosition()),
+                MonkeyConverter.convertToMonkey3D(cannonBall.getSize()), this.assetManager,
+                null);
+        rootNode.attachChild(graphicalBall.getNode());
+        cannonBall.addPropertyChangeListener(graphicalBall);
+    }
+
+    private void createItem(Item item) {
+        GraphicalItem graphicalItem = new GraphicalItem(ColorRGBA.randomColor(),
+                MonkeyConverter.convertToMonkey3D(item.getPosition()),
+                MonkeyConverter.convertToMonkey3D(item.getSize()),
+                this.assetManager, blenderBottle.clone(true));
+        rootNode.attachChild(graphicalItem.getNode());
+        item.addPropertyChangeListener(graphicalItem);
+    }
+
     public void propertyChange(PropertyChangeEvent pce) {
         if ("Player Created".equals(pce.getPropertyName())) {
             if (pce.getNewValue().getClass() == Player.class
                     && pce.getOldValue() == null) {
-                // Salvage what gUnit needs to set itself up from player
+
                 Player player = (Player) pce.getNewValue();
-                Unit unit = player.getUnit();
-
-                Vector pos = unit.getPosition();
-                Vector dir = unit.getDirection();
-                Vector size = unit.getSize();
-                // Also send playerID so it knows which unit to listen to.
-                int playerID = player.getId();
-                
-                final ColorRGBA color = MonkeyConverter.convertToMonkeyColor(player.getColor());
-                this.createGraphicalUnit(playerID, pos, dir, size, color);
-
-                // If a player is created we need to start listening to it so we can know when it shoots
-                player.addPropertyChangeListener(this);
-                
-                Element bar = guiControl.getHealthBarElement(playerID);
-                bar.getParent().setVisible(true);
-                hpBars.put(bar, game.getPlayer(playerID).getUnit());
-                
-                // Show leave button
-                guiControl.playerActive(playerID, true);
-                
-                if (game.hasValidAmountOfPlayers()) {
-                    guiControl.gameValid(true);
-                }
-
+                createPlayer(player);
             } else {
                 throw new RuntimeException(
                         "Unit Created-event sent without correct parameters");
             }
         }
-        
+
         if ("Player Removed".equals(pce.getPropertyName())) {
-            int id = Integer.parseInt(""+pce.getNewValue());
-            guiControl.playerActive(id, false);
-            if (!game.hasValidAmountOfPlayers()) {
-                guiControl.gameValid(false);
-            }
+            removePlayer(Integer.parseInt("" + pce.getNewValue()));
+
         }
 
         if ("CannonBall Created".equals(pce.getPropertyName())) {
             CannonBall cannonBall = (CannonBall) pce.getNewValue();
-            GraphicalCannonBall graphicalBall = new GraphicalCannonBall(ColorRGBA.Orange,
-                    MonkeyConverter.convertToMonkey3D(cannonBall.getPosition()),
-                    MonkeyConverter.convertToMonkey3D(cannonBall.getSize()), this.assetManager,
-                    null);
-            rootNode.attachChild(graphicalBall.getNode());
-            cannonBall.addPropertyChangeListener(graphicalBall);
+            createCannonBall(cannonBall);
         }
 
         if ("Item Created".equals(pce.getPropertyName())) {
             Item item = (Item) pce.getNewValue();
-            GraphicalItem graphicalItem = new GraphicalItem(ColorRGBA.randomColor(),
-                    MonkeyConverter.convertToMonkey3D(item.getPosition()),
-                    MonkeyConverter.convertToMonkey3D(item.getSize()),
-                    this.assetManager, blenderBottle.clone(true));
-            rootNode.attachChild(graphicalItem.getNode());
-            item.addPropertyChangeListener(graphicalItem);
+            createItem(item);
         }
-        
+
         if ("Round Countdown".equals(pce.getPropertyName())) {
-            float counter = Float.parseFloat(""+pce.getNewValue());
+            float counter = Float.parseFloat("" + pce.getNewValue());
             guiControl.countdown(counter);
-            
+
         }
     }
 }
